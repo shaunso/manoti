162542355aa53679b2da68df599329d84bfd38b2
Shaunso <shaunsom@gmail.com>
2025-01-21 21:39:32 +0200

    rollup.js bundling d3 functions for line chart tooltip

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+'use strict';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: 'model/d3/LineChartTooltipBundle.js',
+  output: {
+    file: 'public/lineChartFunctions.min.js',
+    format: 'umd',
+    compact: false,
+    name: 'v'
+  },
+  plugins: [
+    resolve(),
+    terser(),
+  ],
+  // upstream d3 rollup.config.js has this as well.
+  onwarn( message, warn ) {
+    if ( message.code === 'CIRCULAR_DEPENDENCY' ) {
+      return;
+    }
+    warn( message );
+  }
+};
