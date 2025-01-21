787e0d74e4e9cc586bf7d636a60c835d55929373
Shaunso <shaunsom@gmail.com>
2025-01-21 22:19:51 +0200

    fixing bugs to robots.txt sitemap.xml & changing file path for logger.js

--- model/logger.js
+++ model/logger.js
@@ -1,1 +1,2 @@
-import fs from"fs";const logger=(e,r,t)=>{const a=new Date,s=fs.createWriteStream("./utils/logs.txt",{flags:"a"});s.write(`\n>>>>>>>>> \n${a}, ${e.headers["x-real-ip"]},${e.rawHeaders}\n`),s.end(),t()};export default logger;
+import fs from"fs";const logger=(e,r,t)=>{const a=new Date,s=fs.createWriteStream("./utils/logs2.txt",{flags:"a"});s.write(`\n>>>>>>>>> \n${a}, ${e.headers["x-real-ip"]},${e.rawHeaders}\n`),s.end(),t()};export default
+logger;
