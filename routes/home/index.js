d110d9c481a10b3989b1a676e0557d243e499bc3
Shaunso <shaunsom@gmail.com>
2025-03-18 01:07:21 +0200

    v5

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,16 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
-<<<<<<< HEAD
 import { alsi, equityData, heatMap, heatMapData, lastTradingDayDateFormatted, lastTradingDayDateFormattedReversed, marketCapData, turnover30dayValue, medianPEratio, numberOfListedEquities, pieChart, timeline, tooltipData, treemap, vfexEquitiesEndOfDayData, vfexMarketCap, year } from './app.js'
-=======
-import { equityData, lastTradingDayDateFormatted, lastTradingDayDateFormattedReversed, year, tooltipData, alsi, marketCapData, vfexMarketCap, pieChart, heatMap, heatMapData, numberOfListedEquities, marketTurnover1day, marketTurnover50dayAverage, timeline, medianPEratio } from './app.js'
->>>>>>> 35b6819c08f5fa8738fbe628c95ed2f7bca4d95d
 
 router.get( '/', async ( req, res ) => {
   try {
     // send the data to ejs to populate res page
-<<<<<<< HEAD
     res.status(200).render('home', { 
       alsi,
       equityData,
@@ -30,9 +25,6 @@ router.get( '/', async ( req, res ) => {
       vfexMarketCap,
       year
     });    
-=======
-    res.status(200).render('home', { equityData, lastTradingDayDateFormatted, lastTradingDayDateFormattedReversed, year, tooltipData, alsi, marketCapData, vfexMarketCap, pieChart, heatMap, heatMapData, numberOfListedEquities, marketTurnover1day, marketTurnover50dayAverage, timeline, medianPEratio });    
->>>>>>> 35b6819c08f5fa8738fbe628c95ed2f7bca4d95d
   } 
   catch(err) {
     // error page for internal errors
@@ -40,8 +32,4 @@ router.get( '/', async ( req, res ) => {
   }
 });
 
-<<<<<<< HEAD
 export default router;
-=======
-export default router;
->>>>>>> 35b6819c08f5fa8738fbe628c95ed2f7bca4d95d
